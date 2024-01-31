efbefcbef715d76739c1bcc3c034b3c5e09f1c8b
Sergey Starukhin <star.s@me.com>
2024-01-31 19:19:18 +0100

    fix warning

--- Sources/URLEncodedForm/Core/LosslessDataConvertible.swift
+++ Sources/URLEncodedForm/Core/LosslessDataConvertible.swift
@@ -45,7 +45,7 @@ extension String: LosslessDataConvertible {
 extension Array: LosslessDataConvertible where Element == UInt8 {
     /// Converts this `[UInt8]` to `Data`.
     public func convertToData() -> Data {
-        return Data(bytes: self)
+        return Data(self)
     }
 
     /// Converts `Data` to `[UInt8]`.

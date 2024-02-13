cf83096a8302ba8628e4ac920abccb6bf232b624
Sergey Starukhin <star.s@me.com>
2024-02-13 15:58:11 +0100

    add header authorization using AccessToken

--- Sources/HttpClient/Xtensions/OAuth2/Models/Credentials/Credentials.swift
+++ Sources/HttpClient/Xtensions/OAuth2/Models/Credentials/Credentials.swift
@@ -11,3 +11,14 @@ import HttpClientUtilities
 public protocol Credentials {
     func authorize(headers: HTTPHeaders) throws -> HTTPHeaders
 }
+
+public extension Credentials where Self: AccessToken {
+    func authorize(headers: HTTPHeaders) throws -> HTTPHeaders {
+        guard tokenType == .bearer else {
+            return headers
+        }
+        var headers = headers
+        headers.add(.authorization(bearerToken: accessToken))
+        return headers
+    }
+}

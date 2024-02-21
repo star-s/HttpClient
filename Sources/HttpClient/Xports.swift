bc2d378436d7cdb07a6f4784abaee0d1f46bdbb3
Sergey Starukhin <star.s@me.com>
2024-02-21 12:44:03 +0100

    add exports

---
+++ Sources/HttpClient/Xports.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Starukhin on 21.02.2024.
+//
+
+#if !HTTP_CLIENT_DISABLE_EXPORTS
+@_exported import HttpClientUtilities
+@_exported import URLEncodedForm
+#endif
